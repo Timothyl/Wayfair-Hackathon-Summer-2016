09a3e74b9699ea8f78b6b4a42ee3533fde65805d
Timothy <timothyjlau@gmail.com>
2016-07-09 00:23:48 -0400

    twitter api hooked up

---
+++ sentiment_analysis/config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+$twitter = Twitter::REST::Client.new do |config|
+  config.consumer_key = '3BGifa6EN7RMcsLq3spq2x7ty'
+  config.consumer_secret = 'DgZP9TX5zi5bXGBZWRoGrKtWdLJ22tv925iNLqOZHotPA2oIpx'
+  config.access_token = '2809520565-gC7J9HqxvxNQAHEgPNX44JlqS0HRSrr3tspuU31'
+  config.access_token_secret = 'qcvIKnH72f0qujVei1xWBVGpvQyCZHZjoqw9grCPrWH4I'
+end
